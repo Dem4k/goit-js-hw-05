349d38c94bb001eabe24933ab98f59a3e3d0f151
Dem4k <demch1kdanya@gmail.com>
2024-05-22 15:25:39 +0300

    dz5.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,64 +1,60 @@
-
-const allUsers  = [
-    { 
-     name:  "Moore Hensley" ,
-      friends: [ "Sharron Pace" ]
-    } ,
-    { 
-     name:  "Sharlene Bush" ,
-      friends: [ "Briana Decker" , "Sharron Pace" ]
-    } ,
-    { 
-     name:  " Ross Vazquez" ,
-      пользователи: [ "Marilyn Mcintosh" , "Padilla Garrison" , "Naomi Buckner" ]
-    } ,
-    { 
-     name:  "Elma Head" ,
-      friends: [ "Goldie Gentry" , "Aisha Tran" ]
-    } ,
-    { 
-     name:  "Carey Barr" ,
-      друзей: [ "Jordan Sampson" , "Eddie Strong" ]
-    } ,
-    { 
-     name:  "Blackburn Dotson" ,
-      friends: [ "Jacklyn Lucas" , "Linda Chapman" ]
-    } ,
-    { 
-     name:  "Sheree Antho" ,
-      friends: [ "Goldie Gentry" , "Briana Decker" ]
-    } 
- ] ;
- 
-
+const users  = [
+  { 
+   name: "Moore Hensley",
+   friends: ["Sharron Pace"]
+  },
+  { 
+   name: "Sharlene Bush",
+   friends: ["Briana Decker", "Sharron Pace"]
+  },
+  { 
+   name: "Ross Vazquez",
+   friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
+  },
+  { 
+   name: "Elma Head",
+   friends: ["Goldie Gentry", "Aisha Tran"]
+  },
+  { 
+   name: "Carey Barr",
+   friends: ["Jordan Sampson", "Eddie Strong"]
+  },
+  { 
+   name: "Blackburn Dotson",
+   friends: ["Jacklyn Lucas", "Linda Chapman"]
+  },
+  { 
+   name: "Sheree Anthony",
+   friends: ["Goldie Gentry", "Briana Decker"]
+  }
+];
 
 const getUsersWithFriend = (users, friendName) => {
-    return users.filter(user => user.friends.includes(friendName));
+  return users.filter(user => user.friends.includes(friendName));
 };
 
- console.log(getUsersWithFriend(allUsers, "Briana Decker")) ;  
- // [ 
- // { 
- // name: "Sharlene Bush", 
- // friends: ["Briana Decker", "Sharron Pace"] 
- // }, 
- // { 
- // name: "Sheree Anthony", 
- // friends: ["Goldie Gentry", "Briana Decker"] 
- // } 
- // ]
- 
- console.log(getUsersWithFriend(allUsers, "Goldie Gentry")) ; 
- // [ 
- // { 
- // name: "Elma Head", 
- // friends: ["Goldie Gentry", "Aisha Tran"] 
- // }, 
- // { 
- // name: "Sheree Anthony", 
- // friends: ["Goldie Gentry", "Briana Decker"] 
- // } 
- // ]
- 
- console.log(getUsersWithFriend(allUsers, "Adrian Cross")) ;  // []
+console.log(getUsersWithFriend(users, "Briana Decker"));
+// [
+// { 
+// name: "Sharlene Bush",
+// friends: ["Briana Decker", "Sharron Pace"]
+// },
+// { 
+// name: "Sheree Anthony",
+// friends: ["Goldie Gentry", "Briana Decker"]
+// }
+// ]
 
+console.log(getUsersWithFriend(users, "Goldie Gentry"));
+// [
+// { 
+// name: "Elma Head",
+// friends: ["Goldie Gentry", "Aisha Tran"]
+// },
+// { 
+// name: "Sheree Anthony",
+// friends: ["Goldie Gentry", "Briana Decker"]
+// }
+// ]
+
+console.log(getUsersWithFriend(users, "Adrian Cross")); // []
